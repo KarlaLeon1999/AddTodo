876d6bac1bc6c076dda03460fca6dde9ad70bbde
Karla V León <78186389+KarlaLeon1999@users.noreply.github.com>
2021-02-27 12:42:30 -0600

    Update blog.js

--- blog.js
+++ blog.js
@@ -1,5 +1,4 @@
 /**
- *
  * @param {Object} params
  * @param {Array} params.posts // The array of posts to modify
  * @param {String} params.author
@@ -10,9 +9,8 @@
  */
 const addPost = params => {
     const { posts=[], author, text, createdAt, categories} = params
-    // Lógica
     posts.push({author, text, createdAt, categories});
-    return posts
+        return posts
    }
 
    /**
@@ -25,47 +23,44 @@ const addPost = params => {
     * @param {Number} params.index
     * Función que servirá para editar la información de un post
     */
-   const editPost = params => {
+const editPost = params => {
     const { posts=[], author, text, createdAt, categories, index} = params
-    posts [index]= ({author,text,createdAt,categories})
-    return posts
-   }
+    posts[index] ? posts [index]= ({author,text,createdAt,categories}) : alert ('Hey!, ese indice no existe')
+        return posts
+    }
+    //para ejecutar en consola chrome : posts = editPost ({posts,...post1,text: 'Ricardo',index: 0})
    /**
-    *
     * @param {Object} params
     * @param {Array} params.posts // The array of posts to modify
     * @param {Number} params.index
     * Función que servirá para borrar la información de un post
     */
-   const deletePost = params => {
+const deletePost = params => {
     const { posts=[], index } = params
-   delete posts[index]
-    // Lógica
-    return posts
-   }
+    posts[index] ? delete posts[index] : alert ('Error,No Existe')
+        return posts
+    }
+    //para ejecutar en consola chrome :posts = deletePost ({posts,...post1,index:0})
    /**
-    *
     * @param {Object} params
     * @param {Array} params.posts // The array of posts to read
     * @param {Number} params.index
     * Función que servirá para traer la información de solo un post
     */
-   const getPost = params => {
+const getPost = params => {
     const {posts= [], index} = params
-   //return (posts[index]) //funciona pero solo para mandarlo a hacer una vez... no dos... checar
-   console.log (posts[index])//funcion ,aunque creo que el return esta de más
-    // Lógica
-    return posts
-   }
+    posts[index] ? console.log (posts[index]) : alert ('El valor del index no existe')
+        return posts
+    }  
+    //para ejecutar en consola chrome : posts = getPost ({posts,...post1,index:0})
    /**
-    *
     * @param {Object} params
     * @param {Array} params.posts Array of posts
     * @param {String} params.key La key es un string que dirá cómo se
    puede ordernar. Ej key: 'author'. key: 'createdAt'
     * Función que servirá para ordernar todos los posts
     */
-   const sortPostsauthor = params => {
+const sortPostsauthor = params => {
     const {posts = [], key} = params
     posts.sort(function (a,b,key = 'author') {
         if (a[key] > b[key]) {
@@ -74,26 +69,25 @@ const addPost = params => {
         if (a[key] < b[key]) {
             return -1
         }
-        return 0
-    })
-    return posts
-    }
-    const sortPostscreatedAt = params => {
-        const {posts = [], key} = params
-        posts.sort(function (a,b,key = 'createdAt') {
-            if (a[key] > b[key]) {
-               return 1 
-            }
-            if (a[key] < b[key]) {
-                return -1
-            }
             return 0
-        })
+    })
         return posts
+    }
+const sortPostscreatedAt = params => {
+    const {posts = [], key} = params
+    posts.sort(function (a,b,key = 'createdAt') {
+        if (a[key] > b[key]) {
+            return 1 
         }
-
+        if (a[key] < b[key]) {
+            return -1
+        }
+            return 0
+    })
+        return posts
+    }
+        // para ejecutar en consola chrome : posts = sortPostsauthor ({posts,key:'author'})
    /**
-    *
     * @param {Object} params
     * @param {Array} params.posts Array of posts
     * @param {Array} params.categories Array of Strings. Arreglo que
@@ -103,17 +97,15 @@ const addPost = params => {
     * Función que servirá para traer todos los posts que estén dentro
    de esas categorías.
     */
-   const filterPosts = params => {
+const filterPosts = params => {
     const {posts = [], categories = []} = params
-   var entradasInvalidas = 0
-   posts .filter(function (obj) {
-       if ('categories' in obj && obj.categories=== categories) {
-           return true;          
-       }else{
-       entradasInvalidas++
-       }
-   })
-   console.log ('numero de entradas Invalidas = ', entradasInvalidas)
-    return posts // Retornar el arreglo con las categorías
-   }
-   
+    let postsCategories = [];
+        for (let i = 0; i < posts.length; i++) {
+            if (posts[i].categories.includes("general","música")) {
+                postsCategories.push(posts[i]);
+                }
+                }
+                console.log(postsCategories);
+            return posts // Retornar el arreglo con las categorías
+        }
+        //para ejecutar en consola chrome : posts = filterPosts ({posts,categories:['general','música']})
